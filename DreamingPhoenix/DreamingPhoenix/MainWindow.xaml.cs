a6f929eb79d74f5f3239b2ac09dc0671a8f6c8d3
Luck_LATL <luckspamt@gmail.com>
2020-12-24 13:39:55 +0100

    Added new styles and basic adjustments of the audio

--- DreamingPhoenix/DreamingPhoenix/MainWindow.xaml.cs
+++ DreamingPhoenix/DreamingPhoenix/MainWindow.xaml.cs
@@ -20,9 +20,31 @@ namespace DreamingPhoenix
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public AudioHandling.AudioTrack a1 { get; set; } = new AudioHandling.AudioTrack(@"D:\Music\Soundboard\Files\Directed by Robert B. Weide- theme meme.mp3", "Theme"); 
+        public AudioHandling.AudioTrack a2;
+
+        public AppModel AppModelInstance { get; set; } = AppModel.Instance;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+            // a1 =;
+            a1.Volume = 0.1f;
+            a1.FadeOutSpeed = 5000;
+
+            a2 = new AudioHandling.AudioTrack(@"D:\Music\Soundboard\Files\Discord Call Ringtone Remix - Discord Call Ringtone Remix.mp3", "Discord");
+            a2.Volume = 0.1f;
+            a2.FadeOutSpeed = 5000;
+
+            AppModelInstance.AudioList.Add(a1);
+            AppModelInstance.AudioList.Add(a2);
+            // AppModelInstance.AudioList.Add(new AudioHandling.SoundEffect(@"D:\Music\Soundboard\Files\badumtss.swf.mp3", "Badummmts"));
+
+            grid_selectedAudioProperties.Children.Clear();
+            grid_selectedAudioProperties.Children.Add(new UserControls.AudioTrackProperties(a1));
         }
     }
 }
---
+++ DreamingPhoenix/DreamingPhoenix/UserControls/AudioTrackProperties.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DreamingPhoenix.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AudioTrackProperties.xaml
+    /// </summary>
+    public partial class AudioTrackProperties : UserControl
+    {
+        public AudioHandling.AudioTrack Track { get; set; }
+
+        public AudioTrackProperties(AudioHandling.AudioTrack audioTrack)
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            Track = audioTrack;
+        }
+    }
+}
