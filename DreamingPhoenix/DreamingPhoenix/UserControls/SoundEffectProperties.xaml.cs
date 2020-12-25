831dc54ca8b1c3f1ed0d1c5f9bf3a14fdc461f82
Dominic Saladin <dominic@saladin.li>
2020-12-25 02:35:12 +0100

    Added Soundeffect properties usercontrol

---
+++ DreamingPhoenix/DreamingPhoenix/UserControls/SoundEffectProperties.xaml.cs
@@ -0,0 +1,93 @@
+﻿using DreamingPhoenix.AudioHandling;
+using DreamingPhoenix.Styles.ComboBox;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Linq;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DreamingPhoenix.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SoundEffectProperties.xaml
+    /// </summary>
+    public partial class SoundEffectProperties : UserControl, INotifyPropertyChanged
+    {
+        public SoundEffect Sound { get; set; }
+
+        private List<FileExtension> extensions = new List<FileExtension>()
+        {
+            new FileExtension("wav"),
+            new FileExtension("aiff"),
+            new FileExtension("mp3"),
+            new FileExtension("wma"),
+            new FileExtension("aac")
+        };
+
+        public SoundEffectProperties(SoundEffect soundEffect)
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            Sound = soundEffect;
+        }
+
+        private void tbx_soundFile_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files[0] != "" && FileExtension.EndsWith(extensions, files[0]))
+                e.Handled = true;
+        }
+
+        private void tbx_soundFile_Drop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files[0] != "" && FileExtension.EndsWith(extensions, files[0]))
+                Sound.AudioFile = files[0];
+        }
+
+        private void btn_selectFile_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog FileDialog = new System.Windows.Forms.OpenFileDialog()
+            {
+                RestoreDirectory = true,
+                DefaultExt = "mp3",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Filter = "Audio Files|" + FileExtension.GetDialogExtensions(extensions)
+            };
+            FileDialog.Title = "Select Object File";
+            if (FileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                Sound.AudioFile = FileDialog.FileName;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            if (Sound == null)
+                return;
+
+            AppModel.Instance.AudioManager.PlayAudio(Sound);
+        }
+
+        private void DeleteSound_Click(object sender, RoutedEventArgs e)
+        {
+            AppModel.Instance.AudioList.Remove(Sound);
+        }
+    }
+}
