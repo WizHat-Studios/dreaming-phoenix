ba2718cb7d84db70dda9e5a901faeb6d91be5346
Luck_LATL <luckspamt@gmail.com>
2020-12-30 00:09:12 +0100

    Fixed repeat button still enabled when no track playing

---
+++ DreamingPhoenix/DreamingPhoenix/Converter/IsDefaultAudioToBooleanConverter.cs
@@ -0,0 +1,33 @@
+﻿using DreamingPhoenix.AudioHandling;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace DreamingPhoenix.Converter
+{
+    public class IsDefaultAudioToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.GetType() == typeof(AudioTrack))
+            {
+                if ((AudioTrack)value == AudioTrack.Default)
+                    return false;
+            }
+            else if (value.GetType() == typeof(SoundEffect))
+            {
+                if ((SoundEffect)value == SoundEffect.Default)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
