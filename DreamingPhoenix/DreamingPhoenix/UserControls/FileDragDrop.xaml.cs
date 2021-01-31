5a96219c7f5e38c6f1a1d85e78a8f1165aacc1e5
Dominic Saladin <dominic@dsaladin.ch>
2021-01-31 17:49:18 +0100

    Added convert window on import

--- DreamingPhoenix/DreamingPhoenix/UserControls/FileDragDrop.xaml.cs
+++ DreamingPhoenix/DreamingPhoenix/UserControls/FileDragDrop.xaml.cs
@@ -2,9 +2,12 @@
 using NAudio.Wave;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,27 +22,75 @@ namespace DreamingPhoenix.UserControls
     /// <summary>
     /// Interaction logic for FileDragDrop.xaml
     /// </summary>
-    public partial class FileDragDrop : UserControl
+    public partial class FileDragDrop : UserControl, INotifyPropertyChanged
     {
         private List<string> droppedFiles = new List<string>();
 
         public event EventHandler AudioFilesProcessed;
 
+        private string directoryPath;
+
+        public string DirectoryPath
+        {
+            get { return directoryPath; }
+            set
+            {
+                directoryPath = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string fileName;
+
+        public string FileName
+        {
+            get { return fileName; }
+            set
+            {
+                fileName = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string audioName;
+
+        public string AudioName
+        {
+            get { return audioName; }
+            set
+            {
+                audioName = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string fileNameExt;
+
+        public string FileNameExt
+        {
+            get { return fileNameExt; }
+            set
+            {
+                fileNameExt = value;
+                NotifyPropertyChanged();
+            }
+        }
+
         public FileDragDrop()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-
             if (tglbtn_audioTrack.IsChecked == true)
             {
-                AppModel.Instance.AudioList.Add(new AudioHandling.AudioTrack(droppedFiles[0], tbox_newFileName.Text));
+                AppModel.Instance.AudioList.Add(new AudioHandling.AudioTrack(droppedFiles[0], AudioName));
             }
             else if (tglbtn_soundEffect.IsChecked == true)
             {
-                AppModel.Instance.AudioList.Add(new AudioHandling.SoundEffect(droppedFiles[0], tbox_newFileName.Text));
+                AppModel.Instance.AudioList.Add(new AudioHandling.SoundEffect(droppedFiles[0], AudioName));
             }
 
             droppedFiles.RemoveAt(0);
@@ -74,21 +125,24 @@ namespace DreamingPhoenix.UserControls
                 return;
             }
 
+            FileName = Path.GetFileNameWithoutExtension(path);
+            AudioName = Path.GetFileNameWithoutExtension(path);
+            FileNameExt = Path.GetFileName(path);
+            //lbl_fileName.Content = Path.GetFileNameWithoutExtension(path);
+            //lbl_fileName.ToolTip = Path.GetFileName(path);
+            //lbl_fileNameConvert.Content = Path.GetFileNameWithoutExtension(path);
+            //lbl_fileNameConvert.ToolTip = Path.GetFileName(path);
+            //tbox_newFileName.Text = Path.GetFileNameWithoutExtension(path);
+
+            DirectoryPath = Path.GetDirectoryName(path);
+            //lbl_filePath.Content = Path.GetDirectoryName(path);
+            //lbl_filePathConvert.Content = Path.GetDirectoryName(path);
+
             if (GetSampleRate(path) != 44100)
             {
-                // Ask for conversion
-                // MessageBox.Nivisan();
-                if (true)
-                {
-                    if (ConvertToSampleRate(ref path))
-                        droppedFiles[0] = path;
-                }
+                grd_audio.Visibility = Visibility.Hidden;
+                grd_convert.Visibility = Visibility.Visible;
             }
-
-            lbl_fileName.Content = Path.GetFileNameWithoutExtension(path);
-            tbox_newFileName.Text = Path.GetFileNameWithoutExtension(path);
-
-            lbl_filePath.Content = Path.GetDirectoryName(path);
         }
 
         private void SoundEffect_Clicked(object sender, RoutedEventArgs e)
@@ -105,6 +159,57 @@ namespace DreamingPhoenix.UserControls
                 return;
 
             tglbtn_soundEffect.IsChecked = !tglbtn_audioTrack.IsChecked;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            droppedFiles.RemoveAt(0);
+            ProcessNextFile();
+
+            // for better animation, only switch grid if not last file
+            if (droppedFiles.Count != 0)
+            {
+                grd_audio.Visibility = Visibility.Visible;
+                grd_convert.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Convert_Click(object sender, RoutedEventArgs e)
+        {
+            pgb_converting.Visibility = Visibility.Visible;
+            string path = droppedFiles[0];
+
+            Task.Run(() =>
+            {
+                if (!ConvertToSampleRate(ref path))
+                {
+                    droppedFiles.RemoveAt(0);
+                    ProcessNextFile();
+
+                    pgb_converting.Dispatcher.Invoke(() => pgb_converting.Visibility = Visibility.Collapsed);
+
+                    if (droppedFiles.Count != 0)
+                    {
+                        pgb_converting.Dispatcher.Invoke(() =>
+                        {
+                            grd_audio.Visibility = Visibility.Visible;
+                            grd_convert.Visibility = Visibility.Hidden;
+                        });
+                    }
+
+                    return;
+                }
+
+                droppedFiles[0] = path;
+                FileName = Path.GetFileNameWithoutExtension(path);
+                FileNameExt = Path.GetFileName(path);
+                pgb_converting.Dispatcher.Invoke(() =>
+                {
+                    pgb_converting.Visibility = Visibility.Collapsed;
+                    grd_audio.Visibility = Visibility.Visible;
+                    grd_convert.Visibility = Visibility.Hidden;
+                });
+            });
         }
 
         private int GetSampleRate(string fileName)
@@ -164,7 +269,12 @@ namespace DreamingPhoenix.UserControls
                 readerStream = new MediaFoundationReader(fileName);
             }
 
-            string outFile = Path.Combine(Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName) + ".wav");
+            string outFile = Path.Combine(Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName) + "_Convert.wav");
+            if (File.Exists(outFile))
+            {
+                MessageBox.Show(string.Format("File {0} already exists", outFile));
+                return false;
+            }
             int outRate = 44100;
             if (readerStream.WaveFormat.SampleRate == outRate)
             {
@@ -184,6 +294,12 @@ namespace DreamingPhoenix.UserControls
 
             fileName = outFile;
             return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
