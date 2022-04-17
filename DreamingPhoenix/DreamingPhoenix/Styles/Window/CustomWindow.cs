2202373c7784c9ec6af59ff8a423a24ad00a5d53
Dominic Saladin <dominic@dsaladin.ch>
2022-04-17 11:41:32 +0200

    Added Windows 11 support

--- DreamingPhoenix/DreamingPhoenix/Styles/Window/CustomWindow.cs
+++ DreamingPhoenix/DreamingPhoenix/Styles/Window/CustomWindow.cs
@@ -1,12 +1,32 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using static WizHat.DreamingParrot.ParrotWindows.ParrotWindow;
 
 namespace WizHat.DreamingPhoenix.Styles
 {
     public partial class CustomWindow : ResourceDictionary
     {
+        internal static bool IsWindows11 => Environment.OSVersion.Version.Build >= 22000;
+
         public CustomWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            IntPtr hWnd = new WindowInteropHelper((Window)sender).EnsureHandle();
+            var attribute = DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE;
+            var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
+            DwmSetWindowAttribute(hWnd, attribute, ref preference, sizeof(uint));
+
+            if (!IsWindows11)
+            {
+                ((Window)sender).SetResourceReference(Control.BorderBrushProperty, "AccentE");
+            }
         }
 
         private void CloseClick(object sender, RoutedEventArgs e)
@@ -46,5 +66,30 @@ namespace WizHat.DreamingPhoenix.Styles
                 window.BorderThickness = new Thickness(0);
             }
         }
+
+        #region Windows 11
+        // The enum flag for DwmSetWindowAttribute's second parameter, which tells the function what attribute to set.
+        public enum DWMWINDOWATTRIBUTE
+        {
+            DWMWA_WINDOW_CORNER_PREFERENCE = 33
+        }
+
+        // The DWM_WINDOW_CORNER_PREFERENCE enum for DwmSetWindowAttribute's third parameter, which tells the function
+        // what value of the enum to set.
+        public enum DWM_WINDOW_CORNER_PREFERENCE
+        {
+            DWMWCP_DEFAULT = 0,
+            DWMWCP_DONOTROUND = 1,
+            DWMWCP_ROUND = 2,
+            DWMWCP_ROUNDSMALL = 3
+        }
+
+        // Import dwmapi.dll and define DwmSetWindowAttribute in C# corresponding to the native function.
+        [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern long DwmSetWindowAttribute(IntPtr hwnd,
+                                                         DWMWINDOWATTRIBUTE attribute,
+                                                         ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute,
+                                                         uint cbAttribute);
+        #endregion
     }
 }
